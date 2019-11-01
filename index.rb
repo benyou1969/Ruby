707945d89ed07654692ec4f38d8b5f2e8ec72dda
benyou1969 <benyou.11@hotmail.com>
2019-11-01 09:51:19 +0100

    Get Input

---
+++ index.rb
@@ -0,0 +1,11 @@
+print "Enter a value "
+first_num = gets.to_i
+
+print "Enter another value "
+second_num = gets.to_i
+
+# the difference between print and puts
+# print won't put a new line after  it prints out your string that on the screen  
+
+puts first_num.to_s + " + " + second_num.to_s + "= " + (first_num + second_num).to_s
+
