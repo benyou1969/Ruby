df0a98961a727517e5313ae29781a73b482282a4
benyou1969 <benyou.11@hotmail.com>
2019-11-01 10:07:00 +0100

    Floats

--- index.rb
+++ index.rb
@@ -1,3 +1,7 @@
-# Integers or fixed numbers can store extremely large numbers, that don't have decimal places
-# They can go well beyond 4,611,686,018,427,387,903
-
+numOne = 1.000
+ 
+# You must put a 0 before your floats
+num99 = 0.999
+# Lets check the accuracy 
+puts numOne.to_s + " - " + num99.to_s + " = " + (numOne - num99).to_s
+# output: 1.0 - 0.999 = 0.0010000000000000009 it almost accurate untill the last degit
