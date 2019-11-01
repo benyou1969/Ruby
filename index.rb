08267bafed51ebb713e8ee73a40c0e4fce348fea
benyou1969 <benyou.11@hotmail.com>
2019-11-01 10:28:16 +0100

    Constants

--- index.rb
+++ index.rb
@@ -1,9 +1,11 @@
-# Every single thing inside ruby is an Object
+# Constant
+# A constant should start with uppercase letter
 
-puts 1.class 
-#Integer
-puts 1.2345.class
-#Float
-puts "String".class
-#String
+PI = 3.14159265359
+PI = 3.15 # when we say constant that doesn't mean we can't change the value of it but instead will get an error
 
+puts PI
+
+#ndex.rb:5: warning: already initialized constant PI
+#index.rb:4: warning: previous definition of PI was here
+#3.15
