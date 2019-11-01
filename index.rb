2cd096f577318330d89b2bea44fa194cd2c7b896
benyou1969 <benyou.11@hotmail.com>
2019-11-01 11:29:56 +0100

    If Elseif Else

--- index.rb
+++ index.rb
@@ -1,7 +1,8 @@
-# Multi Line Comment
-=begin
-This is a Multiline Comment
-so feel free to break new line
-voila
-=end
-
+age = 21
+if (age >= 5) && (age <= 6)
+ puts "You're in Kindergraten"
+elseif (age >= 7) && (age <= 13)
+ puts "You're in Middle School"
+else 
+ puts "Stay Home"
+end
