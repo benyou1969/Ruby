4d73c9ba4e82d94f2ea0d793508d6dd861cd979c
benyou1969 <benyou.11@hotmail.com>
2019-11-01 10:58:46 +0100

    Load file

--- index.rb
+++ index.rb
@@ -1,9 +1,3 @@
 # File I/O
-
-write_handler = File.new("Greeting.out", "w")
-
-write_handler.puts("Hello, There!").to_s
-write_handler.close
-
-data_from_file = File.read("Greeting.out")
-puts "Data From File: " + data_from_file
+#load
+load "rubyOrNotRuby.rb"
