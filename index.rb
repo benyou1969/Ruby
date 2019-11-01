5107c614f387a872f6d6641b78b0d86a9de4469b
benyou1969 <benyou.11@hotmail.com>
2019-11-01 10:22:43 +0100

    Object in Ruby

--- index.rb
+++ index.rb
@@ -1,7 +1,9 @@
-numOne = 1.000
- 
-# You must put a 0 before your floats
-num99 = 0.999
-# Lets check the accuracy 
-puts numOne.to_s + " - " + num99.to_s + " = " + (numOne - num99).to_s
-# output: 1.0 - 0.999 = 0.0010000000000000009 it almost accurate untill the last degit
+# Every single thing inside ruby is an Object
+
+puts 1.class 
+#Integer
+puts 1.2345.class
+#Float
+puts "String".class
+#String
+
