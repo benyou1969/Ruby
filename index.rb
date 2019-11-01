00ce99b67646e1e6eedb76350d703f7c94cd9d81
benyou1969 <benyou.11@hotmail.com>
2019-11-01 09:53:12 +0100

    Arithmetic

--- index.rb
+++ index.rb
@@ -1,11 +1,5 @@
-print "Enter a value "
-first_num = gets.to_i
-
-print "Enter another value "
-second_num = gets.to_i
-
-# the difference between print and puts
-# print won't put a new line after  it prints out your string that on the screen  
-
-puts first_num.to_s + " + " + second_num.to_s + "= " + (first_num + second_num).to_s
-
+# Arithmetic Opertors
+puts "6 + 4 = " + (6+4).to_s
+puts "6 - 4 = " + (6-4).to_s
+puts "6 * 4 = " + (6*4).to_s
+puts "6 / 4 = " + (6/4).to_s
