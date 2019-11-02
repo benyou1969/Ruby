1231306ca7ac40331278fe621f86e030c5cefc4b
benyou1969 <benyou.11@hotmail.com>
2019-11-02 11:40:53 +0100

    Strings

--- index.rb
+++ index.rb
@@ -1,29 +1,40 @@
-# Exception handling
-print "Enter a number: "
-first_num =   gets.to_i
-print "Enter another "
-second_num = gets.to_i
+puts "#{1+11}this called interpolation"
+puts "Add Them #{34+44} \n "
+puts 'Add Them #{34+44} \n '
 
-begin 
- answer = first_num / second_num
-rescue
- puts "You can't divide by zero"
- exit
-end
+multiline_string = <<EOM
+this is very long string
+that contains interpolation 
+like #{33-31} \n
+EOM
 
-puts "#{first_num} / #{second_num} = #{answer}" 
+puts multiline_string
 
+first_name = "Benyou"
+last_name = "Mansour"
+full_name = first_name + last_name
 
-age = 12
+puts full_name.include?("Benyou")
+puts full_name.size
+puts "Vowels: " + full_name.count("eauio").to_s 
+puts "Consaonants: " + full_name.count("^eauio").to_s
+puts full_name.start_with?("Mansour") 
+puts full_name.start_with?("Benyou")
 
-def check_age(age)
-  raise ArgumentError, "Enter Positive number" unless age > 0
-end
+puts "Index: " + full_name.index("Mansour").to_s
+puts first_name.equal?first_name 
 
-begin
- check_age(-1)
-rescue ArgumentError
- puts "That is an impossible age"
-end 
+puts full_name.upcase
+puts full_name.downcase
+puts full_name.swapcase
 
+full_name = "      " + full_name
+
+puts 'lstrip '+ full_name.lstrip
+puts 'rstrip ' + full_name.rstrip
+puts 'strip ' + full_name.strip
+
+puts 'rjust '+ full_name.rjust(20, '.')
+puts 'ljust ' +full_name.ljust(20, '.')
+puts 'center' +full_name.center(20, '.')
 
