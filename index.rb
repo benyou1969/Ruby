fa33fd1bb98eb89ec2996eacfacfe7c6b3044b93
benyou1969 <benyou.11@hotmail.com>
2019-11-02 14:26:05 +0100

    Enumerables

--- index.rb
+++ index.rb
@@ -1,35 +1,28 @@
-# Arrays
+# Hashes are just collection of key value paris
+number_has = {"PI" => 3.14, 
+              "Golden" => 1.618,
+              "e" => 2.718}
+puts number_has["e"]
 
-array_1 = Array.new
-array_2 = Array.new(5)
-array_3 = Array.new(4, "empty")
-array_4 = [1,2,"three", 5.5, 3, 3]
+superheroes = Hash["Clark Kent", "Supperman", "Bruce Wayne", "Batman"]
+puts superheroes["Bruce Wayne"]
 
-puts array_1
-puts array_2 
-puts array_3
-puts array_4
-puts array_4[2, 2].join(", ")
-puts "values " + array_4.values_at(0,1,3).join(", ")
+superheroes["Barry Allen"] = "Flash"
 
-array_4.unshift(9382) #add value
-puts "first item " + array_4.first.to_s
-array_4.shift #remove first value
-array_4.pop #remove last value
-puts "first item " + array_4.first.to_s
+samp_hash = Hash.new("No such key")
+puts samp_hash["Dog"]
 
-array_4.push(1996) 
-array_4.concat([98,3948,"df", 3, 3]) # add item to the end of array
-puts "last item " + array_4.last.to_s
+superheroes = Hash["Clark Kent", "Supperman", "Bruce Wayne", "Batman", "Mansour Benyoucef", "Ben"]
+superheroes.update(superheroes) # update is going to eliminate duplicate value
 
-puts "Array Size: " + array_4.size().to_s
-puts "Array contains 98: " + array_4.include?(98).to_s
-puts "How many 3 : " + array_4.count(3).to_s
-puts "Array Empty: " + array_4.empty?.to_s
-
-puts array_4.join(", ")
-
-p array_4 # to print an array 
-array_4.each do |value|
-  puts value 
+superheroes.each do |key, value|
+   puts key + " => " + value
 end
+
+puts "Has Key Mansour Benyoucef: " + superheroes.has_key?("Mansour Benyoucef").to_s
+puts "Has value Ben: " + superheroes.has_value?("Ben").to_s
+puts "is Hash empty" + superheroes.empty?.to_s
+puts "Size of hash: " + superheroes.size.to_s
+
+superheroes.delete("Mansour Benyoucef")
+puts "Size after deleted " + superheroes.size.to_s
